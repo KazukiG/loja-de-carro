4715db72c7fe7e7e4d596d32ea2fe680d1cfcd51
KazukiG <57380453+KazukiG@users.noreply.github.com>
2020-09-02 20:40:48 -0300

    inicio

---
+++ TrilhaOnline.Presentation.Rent.UI/Controllers/ManagementController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrilhaOnline.Presentation.Rent.UI.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cars()
+        {
+            return View();
+        }
+
+        public ActionResult Parts()
+        {
+            return View();
+        }
+
+        public ActionResult _partialCreateCars()
+        {
+            return View();
+        }
+    }
+}
---
+++ TrilhaOnline.Presentation.Rent.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrilhaOnline.Presentation.Rent.UI.MvcApplication" Language="C#" %>
